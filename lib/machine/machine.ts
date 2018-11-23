08560a2f226d76a3bd6a59224e098164696aef56
Christian Dupuis <cd@atomist.com>
2018-11-23 17:20:33 +0100

    Prepare manifesto-sdm

--- lib/machine/machine.ts
+++ lib/machine/machine.ts
@@ -102,8 +102,8 @@ export function machine(configuration: SoftwareDeliveryMachineConfiguration): So
         configuration,
     },
 
-        whenPushSatisfies(isOrgNamed("sdd-manifesto"))
-            .itMeans("sdd-manifesto org")
+        whenPushSatisfies(allSatisfied(isOrgNamed("sdd-manifesto"), isNamed("manifesto")))
+            .itMeans("Manifesto repository")
             .setGoals(DoNotSetAnyGoals),
 
         whenPushSatisfies(not(IsNode))
@@ -135,7 +135,7 @@ export function machine(configuration: SoftwareDeliveryMachineConfiguration): So
         // Simplified deployment goal set for atomist-sdm, k8-automation; we are skipping
         // testing for these and deploying straight into their respective namespaces
         whenPushSatisfies(IsNode, HasDockerfile, ToDefaultBranch, IsAtomistAutomationClient,
-            isNamed("k8-automation", "atomist-sdm", "docs-sdm"))
+            isNamed("k8-automation", "atomist-sdm", "docs-sdm", "manifesto-sdm"))
             .itMeans("Simplified Deploy")
             .setGoals(SimplifiedKubernetesDeployGoals),
 
