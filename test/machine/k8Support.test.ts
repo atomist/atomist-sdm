14aa294f6b0b3340ce097a018c81af26efe0b04e
David Dooling <david@atomist.com>
2018-12-04 14:18:08 -0600

    Sync up kube specs and Dockerfile
    Update Kubernetes specs to make them consistent with each other and
    the deployed atomist-sdm.  Update the Dockerfile to optimize shared
    layers, used the dumb-init package in Ubuntu, and move customization
    to the bottom.
    
    Update Kubernetes data function to account for three replicas for
    atomist-sdm and add tests.

--- test/machine/k8Support.test.ts
+++ test/machine/k8Support.test.ts
@@ -14,10 +14,283 @@
  * limitations under the License.
  */
 
+import {
+    GitProject,
+    InMemoryProject,
+} from "@atomist/automation-client";
+import {
+    RepoContext,
+    SdmGoalEvent,
+    SoftwareDeliveryMachine,
+} from "@atomist/sdm";
+import { KubernetesApplicationOptions } from "@atomist/sdm-pack-k8";
 import * as assert from "power-assert";
-import { ingressFromGoal } from "../../lib/machine/k8Support";
+import {
+    ingressFromGoal,
+    kubernetesDeploymentData,
+} from "../../lib/machine/k8Support";
 
 describe("k8Support", () => {
+
+    describe("kubernetesDeploymentData", () => {
+
+        function sdmGen(gp: GitProject): SoftwareDeliveryMachine {
+            return {
+                projectLoader: {
+                    doWithProject: async (params: any, action: (p: GitProject) => Promise<KubernetesApplicationOptions>) => action(gp),
+                },
+            } as any;
+        }
+        const context: RepoContext = {
+            credentials: "creds",
+            id: {},
+        } as any;
+
+        it("should provide function that generates Kubernetes deployment data", async () => {
+            const p: GitProject = InMemoryProject.of() as any;
+            const environment = "testing";
+            const sdm: SoftwareDeliveryMachine = {
+                configuration: {
+                    environment,
+                    sdm: sdmGen(p),
+                },
+            } as any;
+            const goal: SdmGoalEvent = {
+                environment,
+                repo: {
+                    name: "rocknroll",
+                },
+            } as any;
+            const d = await kubernetesDeploymentData(sdm)(goal, context);
+            assert(d);
+            const e = {
+                name: "rocknroll",
+                environment,
+                port: 2866,
+                ns: "default",
+                replicas: 1,
+            };
+            assert.deepStrictEqual(d, e);
+        });
+
+        it("should set port for Maven project", async () => {
+            const p: GitProject = InMemoryProject.of({ path: "pom.xml", content: "" }) as any;
+            const environment = "testing";
+            const sdm: SoftwareDeliveryMachine = {
+                configuration: {
+                    environment,
+                    sdm: sdmGen(p),
+                },
+            } as any;
+            const goal: SdmGoalEvent = {
+                environment,
+                repo: {
+                    name: "rocknroll",
+                },
+            } as any;
+            const d = await kubernetesDeploymentData(sdm)(goal, context);
+            assert(d);
+            const e = {
+                name: "rocknroll",
+                environment,
+                port: 8080,
+                ns: "default",
+                replicas: 1,
+            };
+            assert.deepStrictEqual(d, e);
+        });
+
+        it("should detect staging environment", async () => {
+            const p: GitProject = InMemoryProject.of() as any;
+            const sdm: SoftwareDeliveryMachine = {
+                configuration: {
+                    environment: "myk8_testing",
+                    sdm: sdmGen(p),
+                },
+            } as any;
+            const goal: SdmGoalEvent = {
+                environment: "1-staging",
+                repo: {
+                    name: "rocknroll",
+                },
+            } as any;
+            const d = await kubernetesDeploymentData(sdm)(goal, context);
+            assert(d);
+            const e = {
+                name: "rocknroll",
+                environment: "myk8",
+                port: 2866,
+                ns: "testing",
+                replicas: 1,
+            };
+            assert.deepStrictEqual(d, e);
+        });
+
+        it("should detect production environment", async () => {
+            const p: GitProject = InMemoryProject.of() as any;
+            const sdm: SoftwareDeliveryMachine = {
+                configuration: {
+                    environment: "myk8_prod",
+                    sdm: sdmGen(p),
+                },
+            } as any;
+            const goal: SdmGoalEvent = {
+                environment: "2-prod",
+                repo: {
+                    name: "rocknroll",
+                },
+            } as any;
+            const d = await kubernetesDeploymentData(sdm)(goal, context);
+            assert(d);
+            const e = {
+                name: "rocknroll",
+                environment: "myk8",
+                port: 2866,
+                ns: "production",
+                replicas: 3,
+            };
+            assert.deepStrictEqual(d, e);
+        });
+
+        it("should detect atomist-sdm", async () => {
+            const p: GitProject = InMemoryProject.of() as any;
+            const sdm: SoftwareDeliveryMachine = {
+                configuration: {
+                    environment: "myk8_prod",
+                    sdm: sdmGen(p),
+                },
+            } as any;
+            const goal: SdmGoalEvent = {
+                environment: "2-prod",
+                repo: {
+                    name: "atomist-sdm",
+                },
+            } as any;
+            const d = await kubernetesDeploymentData(sdm)(goal, context);
+            assert(d);
+            const e = {
+                name: "atomist-sdm",
+                environment: "myk8",
+                port: 2866,
+                ns: "sdm",
+                replicas: 3,
+            };
+            assert.deepStrictEqual(d, e);
+        });
+
+        it("should detect atomist-internal-sdm", async () => {
+            const p: GitProject = InMemoryProject.of() as any;
+            const sdm: SoftwareDeliveryMachine = {
+                configuration: {
+                    environment: "myk8_prod",
+                    sdm: sdmGen(p),
+                },
+            } as any;
+            const goal: SdmGoalEvent = {
+                environment: "1-staging",
+                repo: {
+                    name: "atomist-internal-sdm",
+                },
+            } as any;
+            const d = await kubernetesDeploymentData(sdm)(goal, context);
+            assert(d);
+            const e = {
+                name: "atomist-internal-sdm",
+                environment: "myk8",
+                port: 2866,
+                ns: "sdm-testing",
+                replicas: 1,
+            };
+            assert.deepStrictEqual(d, e);
+        });
+
+        it("should provide an ingress", async () => {
+            const p: GitProject = InMemoryProject.of() as any;
+            const sdm: SoftwareDeliveryMachine = {
+                configuration: {
+                    environment: "myk8_prod",
+                    sdm: sdmGen(p),
+                },
+            } as any;
+            const goal: SdmGoalEvent = {
+                environment: "1-staging",
+                repo: {
+                    name: "card-automation",
+                },
+            } as any;
+            const d = await kubernetesDeploymentData(sdm)(goal, context);
+            assert(d);
+            const e = {
+                name: "card-automation",
+                environment: "myk8",
+                port: 2866,
+                ns: "testing",
+                replicas: 1,
+                host: "pusher.atomist.services",
+                path: "/",
+                tlsSecret: "star-atomist-services",
+            };
+            assert.deepStrictEqual(d, e);
+        });
+
+        it("should provide a production ingress", async () => {
+            const p: GitProject = InMemoryProject.of() as any;
+            const sdm: SoftwareDeliveryMachine = {
+                configuration: {
+                    environment: "myk8_prod",
+                    sdm: sdmGen(p),
+                },
+            } as any;
+            const goal: SdmGoalEvent = {
+                environment: "2-prod",
+                repo: {
+                    name: "intercom-automation",
+                },
+            } as any;
+            const d = await kubernetesDeploymentData(sdm)(goal, context);
+            assert(d);
+            const e = {
+                name: "intercom-automation",
+                environment: "myk8",
+                port: 2866,
+                ns: "production",
+                replicas: 3,
+                host: "intercom.atomist.com",
+                path: "/",
+                tlsSecret: "star-atomist-com",
+            };
+            assert.deepStrictEqual(d, e);
+        });
+
+        it("should not use TLS for badge", async () => {
+            const p: GitProject = InMemoryProject.of() as any;
+            const sdm: SoftwareDeliveryMachine = {
+                configuration: {
+                    environment: "myk8_prod",
+                    sdm: sdmGen(p),
+                },
+            } as any;
+            const goal: SdmGoalEvent = {
+                environment: "2-prod",
+                repo: {
+                    name: "sdm-automation",
+                },
+            } as any;
+            const d = await kubernetesDeploymentData(sdm)(goal, context);
+            assert(d);
+            const e = {
+                name: "sdm-automation",
+                environment: "myk8",
+                port: 2866,
+                ns: "production",
+                replicas: 3,
+                host: "badge.atomist.com",
+                path: "/",
+            };
+            assert.deepStrictEqual(d, e);
+        });
+
+    });
 
     describe("ingressFromGoal", () => {
 
