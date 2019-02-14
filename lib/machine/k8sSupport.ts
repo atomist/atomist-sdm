c514593244e64ad71bbc85f960ce5ff3912bbbad
Christian Dupuis <cd@atomist.com>
2019-02-14 11:26:07 +0100

    Remove the image pull secret

--- lib/machine/k8sSupport.ts
+++ lib/machine/k8sSupport.ts
@@ -75,9 +75,6 @@ export async function kubernetesApplicationData(
         replicas,
         ...ingress,
     };
-    if (name === "global-sdm") {
-        baseApp.imagePullSecret = "global-sdm-imagepullsecret";
-    }
     return (name === "k8s-sdm") ? addK8sSecret(baseApp, goal, goalEvent.fulfillment.name) : baseApp;
 }
 
