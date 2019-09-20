cba712d357fb8d4059c8b455a969866c6762a655
Christian Dupuis <cd@atomist.com>
2019-09-19 21:36:27 -0700

    Polish transform

--- lib/transform/sourcesTransform.ts
+++ lib/transform/sourcesTransform.ts
@@ -17,6 +17,7 @@
 import {
     LocalProject,
     projectUtils,
+    ProjectFile,
 } from "@atomist/automation-client";
 import { CodeTransform } from "@atomist/sdm";
 import * as fs from "fs-extra";
@@ -28,32 +29,35 @@ import * as path from "path";
 export const SourcesTransform: CodeTransform = async (p, papi) => {
     const cwd = (p as LocalProject).baseDir;
 
-    const files = (await projectUtils.gatherFromFiles(p, ["**/*.ts"], async f => f)).filter(f => !f.path.endsWith(".d.ts"));
+    const files = (await projectUtils.gatherFromFiles(p, ["**/*.ts"], async f => f))
+        .filter(f => !f.path.endsWith(".d.ts"));
+
     if (files.length > 0) {
+        // Make sure the src path exists and we can move files into
         await fs.ensureDir(path.join(cwd, "src"));
+
         for (const file of files) {
+            // Move the ts file out of the way
             await fs.move(path.join(cwd, file.path), path.join(cwd, "src", file.path));
+
+            // Get both map files
             const base = file.path.slice(0, -3);
             const tsMap = path.join(cwd, `${base}.d.ts.map`);
             const jsMap = path.join(cwd, `${base}.js.map`);
 
-            const segments = `..${path.sep}`.repeat(file.path.split("/").length - 1);
-
-            if (await fs.pathExists(tsMap)) {
-                const content = (await fs.readFile(tsMap)).toString();
-                const newContent = content.replace(
-                    /("sources":\[.*?\])/g,
-                    `"sources":["${segments}src${path.sep}${file.path}"]`);
-                await fs.writeFile(tsMap, newContent);
-            }
-
-            if (await fs.pathExists(jsMap)) {
-                const content = (await fs.readFile(jsMap)).toString();
-                const newContent = content.replace(
-                    /("sources":\[.*?\])/g,
-                    `"sources":["${segments}src${path.sep}${file.path}"]`);
-                await fs.writeFile(tsMap, newContent);
-            }
+            await updateSourceMap(tsMap, file);
+            await updateSourceMap(jsMap, file);
         }
     }
 };
+
+async function updateSourceMap(mapPath: string, file: ProjectFile): Promise<void> {
+    const segments = `..${path.sep}`.repeat(file.path.split("/").length - 1);
+    if (await fs.pathExists(mapPath)) {
+        const content = (await fs.readFile(mapPath)).toString();
+        const newContent = content.replace(
+            /("sources":\[.*?\])/g,
+            `"sources":["${segments}src${path.sep}${file.path}"]`);
+        await fs.writeFile(mapPath, newContent);
+    }
+}
